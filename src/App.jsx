f62a2928c1d011ccecb8ae2aa47705d67c2d959e
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-01-21 18:22:23 +0800

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar.jsx';
+import HomePage from '../src/pages/HomePage.jsx';
+import AboutPage from '../src/pages/AboutPage.jsx'
 import './App.css'
 
 function App() {
  
   return (
     <>
-      
+    <NavBar/>
+
+      <Routes>
+        <Route  path='/' element={<HomePage />}/>
+        <Route  path='about' element={<AboutPage />}/>
+      </Routes>
     </>
   )
 }
